c4aa137e7cc5cba71079be5e0b6da8b952b0735a
PernilleLange <pernillel@gmail.comgit config --global --lisy #git config --global user.email pernillel@gmail.com>
2020-09-16 10:13:36 +0200

    Solution to Python Morsels' parse_ranges-problem, full

--- parse_ranges/parse_ranges.py
+++ parse_ranges/parse_ranges.py
@@ -1,18 +1,18 @@
-def parse_ranges(intake):
-    num_list2 = []
+import re
+
+def parse_ranges(feed):
+    intake = re.sub(r'->exit', r'', feed)
+    num_list = []
     for pair in intake.split(','):
         if '-' in pair:
             x,y = pair.split('-')
-            num_list2.append([int(x), int(y)+1])
+            num_list.append([int(x), int(y)+1])
         else:
-           num_list2.append([int(pair), int(pair)+1])
+           num_list.append([int(pair), int(pair)+1])
 
-    for pair in num_list2:
+    for pair in num_list:
         a,b = pair
         numbers = range(a,b)
         for num in numbers:
             yield num
-
-
-
 
